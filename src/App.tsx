bcba77d2aca404bbe99be135306a3a34685e8d0f
Phong <vanphong02062002@gmail.com>
2024-01-15 16:30:06 +0700

    eslint ts, prettier, vitest

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+export default App;
