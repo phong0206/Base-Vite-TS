256ceb22e51c2f33f845e4327ddf3ef6dc42f5d0
Phong <92936587+phong0206@users.noreply.github.com>
2024-01-16 16:03:44 +0700

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -31,6 +31,15 @@ module.exports = {
   plugins: ['prettier', '@typescript-eslint', 'react'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-explicit-any': ['off'],
+    'react/no-unescaped-entities': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'react/jsx-props-no-spreading': 0,
+    '@typescript-eslint/no-shadow': 0,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 0,
+    'import/extensions': 0,
     'prettier/prettier': [
       'error',
       {
